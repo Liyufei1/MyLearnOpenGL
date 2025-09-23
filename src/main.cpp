27d8413d4566b12f2c24cb1e111776a6cd6ee20c
LYF <liyf@ns-iot.com>
2025-09-23 17:30:07 +0800

    feat：：包装一个main类，用于初始化一下通用资产

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,66 @@
 #include "Camera/Camera.h"
 
 
+
+Main& Main::GetInstance(){
+	static  Main s_Instance ;
+	return s_Instance;
+}
+
+void Main::LoadUsedResources(){
+
+	//加载的贴图
+	T_WoodBox = std::shared_ptr<Texture2D>{new Texture2D("E:/study/LearnOpenGL/Art/Texture/T_WoodBox.jpg")};
+	T_SmallFace = std::shared_ptr<Texture2D>{new Texture2D("E:/study/LearnOpenGL/Art/Texture/T_face.png")};
+
+	//着色器程序
+	PhoneShaderPrograme = std::make_shared<ShaderProgram>("src/Shader/PhoneShader/PhoneVertex.glsl","src/Shader/PhoneShader/PhoneFragment.glsl");
+	BaseShaderPrograme = ShaderProgram::GetDefaultShaderProgram();
+}
+
+
+void Main::InitMesh(){
+
+}
+
+void Main::InitLight(){
+	
+	PointLight1.SetDiffuse(glm::vec3(1.0f, 1.0f, 1.0f));
+	PointLight1.SetPosition(glm::vec3(2.0 * sin(0), 2.0*cos(0), 1.0f));
+	PointLight1.AddShaderProgram(PhoneShaderPrograme);
+	SM_PointLight1 = BoxMesh();
+	SM_PointLight1->SetLocation(PointLight1.GetLightPosition());
+	SM_PointLight1->SetShaderProgram(BaseShaderPrograme);
+	SM_PointLight1->SetScale(glm::vec3(0.1f, 0.1f, 0.1f));
+
+	AddUpdateFunction([this](){
+		PointLight1.SetPosition(glm::vec3(2.0 * sin(glfwGetTime()), 2.0*cos(glfwGetTime()), 1.0f));
+		SM_PointLight1->SetLocation(PointLight1.GetLightPosition());
+		PointLight1.UpdateLight();
+	});
+	
+}
+
 int main()
 {
 	Window& window {Window::GetInstance()};
+	Main& main = Main::GetInstance();
+	main.LoadUsedResources();
+
+	main.InitLight();
+
 	TestFun();
 
-	//加载的贴图
-	std::shared_ptr<Texture2D> texture0{new Texture2D("E:/study/LearnOpenGL/Art/Texture/T_WoodBox.jpg")};
-	std::shared_ptr<Texture2D> texture1{new Texture2D("E:/study/LearnOpenGL/Art/Texture/T_face.png")};
 
-	//着色器程序
-	std::shared_ptr<ShaderProgram> PhoneShaderPrograme = std::make_shared<ShaderProgram>("src/Shader/PhoneShader/PhoneVertex.glsl","src/Shader/PhoneShader/PhoneFragment.glsl");
-	std::shared_ptr<ShaderProgram> BaseShaderPrograme = ShaderProgram::GetDefaultShaderProgram();
-	PhoneShaderPrograme->SetParamater("uLightColor", glm::vec3(1,1,1));
-	PhoneShaderPrograme->SetParamater("uLightPos", glm::vec3(0,0,5));
 
-	//Light光照
-	Light Light1;
-	std::shared_ptr<StaticMesh> SMLight1 = BoxMesh();
-	double lightR1 = 2.0f;
-	Light1.SetLightColor(glm::vec3(1.0f, 1.0f, 1.0f));
-	Light1.SetLightPosition(glm::vec3(lightR1 * sin(0), lightR1*cos(0), 1.0f));
-	Light1.AddShaderProgram(PhoneShaderPrograme);
-	SMLight1->SetLocation(Light1.GetLightPosition());
-	SMLight1->SetShaderProgram(BaseShaderPrograme);
-	SMLight1->SetScale(glm::vec3(0.1f, 0.1f, 0.1f));
+	
 
 	std::shared_ptr<StaticMesh> SM = TestMesh0();
-	SM->SetShaderProgram(PhoneShaderPrograme);
+	SM->SetShaderProgram(main.PhoneShaderPrograme);
 	SM->SetLocation(glm::vec3(0.5,0,0));
 	SM->SetScale(glm::vec3(0.5,0.5,0.5));
-	SM->SetTexture(0,texture0);
-	SM->SetTexture(1,texture1);
+	SM->SetTexture(0,main.T_WoodBox);
+	SM->SetTexture(1,main.T_SmallFace);
 
 	std::shared_ptr<StaticMesh> SM1 = TestMesh0();
 	// SM1->SetShaderProgram(BaseShaderPrograme);
@@ -49,14 +77,14 @@ int main()
 	SM1->GetShaderProgram()->SetParamater("uBaseColor", glm::vec4(0.8,0.8,0.8,1));
 
 	std::shared_ptr<StaticMesh> SM2 = BoxMesh();
-	SM2->SetShaderProgram(PhoneShaderPrograme);
+	SM2->SetShaderProgram(main.PhoneShaderPrograme);
 	SM2->SetLocation(glm::vec3(-0.0f,0,-1.5f));
 	SM2->SetScale(glm::vec3(0.5,0.5,0.5));
-	SM2->SetTexture(0,texture0);
-	SM2->SetTexture(1,texture1);
+	SM2->SetTexture(0,main.T_WoodBox);
+	SM2->SetTexture(1,main.T_SmallFace);
 
 	std::shared_ptr<StaticMesh> SM3 = PlaneMesh();
-	SM3->SetShaderProgram(PhoneShaderPrograme);
+	SM3->SetShaderProgram(main.PhoneShaderPrograme);
 	SM3->SetLocation(glm::vec3(0.0f,0.0f,-2.0f));
 	SM3->SetScale(glm::vec3(2.5,2.5,2.5));
 
@@ -69,9 +97,10 @@ int main()
 	window.SetRunFunction([&](){
 		glClearColor(0.0f,0.0f,0.0f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		Light1.SetLightPosition(glm::vec3(lightR1 * sin(glfwGetTime()), lightR1*cos(glfwGetTime()), 1.0f));
-		SMLight1->SetLocation(Light1.GetLightPosition());
-		Light1.UpdateLight();
+
+		main.Update();
+
+		
 		
 		SM->SetRotation(glm::vec3{0.0f,0.0f,90 * std::fmod(glfwGetTime(),4)});
 		SM->GetShaderProgram()->SetParamater<float>("Test", (sin(glfwGetTime())));
--- src/main.h
+++ src/main.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Camera/Camera.h"
 #include "Common/CommonFunLib.h"
 #include "Common/CommonFunLib.hpp"
 #include "Common/Config.h"
@@ -12,9 +13,50 @@
 #include "glm/fwd.hpp"
 #include "glm/glm.hpp"
 #include <cmath>
+#include <cstddef>
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
 
 
 #include "Test.h"
+
+class Light;
+
+class Main
+{
+public:
+    ~Main(){}
+    static Main& GetInstance();
+
+    void Update(){
+        for(auto it :UpdateFunctions) it();
+    }
+
+    void AddUpdateFunction(std::function<void()> func){UpdateFunctions.push_back(func);}
+
+    void LoadUsedResources();
+
+    void InitMesh();
+
+    void InitLight();
+
+    //贴图
+    std::shared_ptr<Texture2D> T_WoodBox = nullptr;
+    std::shared_ptr<Texture2D> T_SmallFace = nullptr;
+
+    //着色器程序
+    std::shared_ptr<ShaderProgram> PhoneShaderPrograme = nullptr;
+    std::shared_ptr<ShaderProgram> BaseShaderPrograme = nullptr;
+
+    //光照
+    Light PointLight1;
+    std::shared_ptr<StaticMesh> SM_PointLight1   = nullptr;
+
+private:
+    std::vector<std::function<void()>> UpdateFunctions;
+    Main(){}
+
+};
+
